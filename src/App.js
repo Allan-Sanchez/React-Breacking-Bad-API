7d89b28ea978a670ca597393143bc8177bc6dde2
Allan-Sanchez <asanchezrixtun@gmail.com>
2020-07-22 16:49:47 -0600

    init proyect breacking bad api cites

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import  styled from '@emotion/styled';
+
+const Container = styled.div`
+  display:flex;
+  align-items:center;
+  padding-top:5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+background: -webkit-linear-gradient(top left,#007d35 0%, #007d35 40%,#0f574e 100%);
+background-size: 300px;
+font-family: Arial, Helvetica, sans-serif;
+color:#fff;
+margin-top:3rem;
+padding: 1rem 3rem;
+font-size: 2rem;
+border: 1px solid black;
+`;
 
 function App() {
+
+  const getCite = () =>{
+  console.log('getting');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Button
+      onClick={getCite}
+      > Get cite</Button>
+    </Container>
   );
 }
 
